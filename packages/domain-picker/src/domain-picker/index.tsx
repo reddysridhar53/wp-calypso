b2ab7fa28a42780ed7cb262662bd7d05e39828dc
Razvan Papadopol <razvan.papadopol@automattic.com>
2020-08-18 21:54:14 +0300

    Use link instead of button for view more results link. - use font variables

--- packages/domain-picker/src/domain-picker/index.tsx
+++ packages/domain-picker/src/domain-picker/index.tsx
@@ -257,7 +257,7 @@ const DomainPicker: FunctionComponent< Props > = ( {
 							allDomainSuggestions?.length &&
 							allDomainSuggestions?.length > quantity && (
 								<div className="domain-picker__show-more">
-									<Button onClick={ () => setIsExpanded( true ) } isSecondary>
+									<Button onClick={ () => setIsExpanded( true ) } isLink>
 										{ __( 'View more results' ) }
 									</Button>
 								</div>
