1b50203c7323bbe38b05bb785c5218d2abe191eb
Payton Swick <payton@foolord.com>
2020-09-11 20:09:25 -0400

    Fixx in Subscriptions component

--- client/my-sites/purchases/subscriptions/index.tsx
+++ client/my-sites/purchases/subscriptions/index.tsx
@@ -3,6 +3,7 @@
  */
 import React from 'react';
 import { useSelector } from 'react-redux';
+import { useTranslate } from 'i18n-calypso';
 
 /**
  * Internal Dependencies
@@ -14,11 +15,16 @@ import PurchasesHeader from './header';
 import PurchasesSite from 'me/purchases/purchases-site/index.jsx';
 import QueryUserPurchases from 'components/data/query-user-purchases';
 import {
+	getSitePurchases,
 	hasLoadedUserPurchasesFromServer,
 	isFetchingUserPurchases,
 } from 'state/purchases/selectors';
 import QueryConciergeInitial from 'components/data/query-concierge-initial';
 import { getCurrentUserId } from 'state/current-user/selectors';
+import { getSelectedSite } from 'state/ui/selectors';
+import NoSitesMessage from 'components/empty-content/no-sites-message';
+import { CompactCard } from '@automattic/components';
+import EmptyContent from 'components/empty-content';
 
 export default function Subscriptions() {
 	const userId = useSelector( ( state ) => getCurrentUserId( state ) );
@@ -35,8 +41,63 @@ export default function Subscriptions() {
 }
 
 function SubscriptionsContent() {
-	if ( isFetchingUserPurchases && ! hasLoadedUserPurchasesFromServer ) {
+	const isFetchingPurchases = useSelector( ( state ) => isFetchingUserPurchases( state ) );
+	const hasLoadedPurchases = useSelector( ( state ) => hasLoadedUserPurchasesFromServer( state ) );
+	const selectedSite = useSelector( ( state ) => getSelectedSite( state ) );
+	const purchases = useSelector( ( state ) => getSitePurchases( state, selectedSite?.ID ) );
+	const translate = useTranslate();
+
+	if ( isFetchingPurchases && ! hasLoadedPurchases ) {
 		return <PurchasesSite isPlaceholder />;
 	}
-	return null;
+
+	if ( hasLoadedPurchases && ! purchases.length && ! selectedSite?.ID ) {
+		return <NoSitesMessage />;
+	}
+
+	if ( hasLoadedPurchases && ! purchases.length ) {
+		// TODO: add concierge banner
+		return (
+			<CompactCard className="subscriptions__list--empty">
+				<EmptyContent
+					title={ translate( 'Looking to upgrade?' ) }
+					line={ translate(
+						'Our plans give your site the power to thrive. ' + 'Find the plan that works for you.'
+					) }
+					action={ translate( 'Upgrade now' ) }
+					actionURL={ '/plans' }
+					illustration={ '/calypso/images/illustrations/illustration-nosites.svg' }
+				/>
+			</CompactCard>
+		);
+	}
+
+	if ( hasLoadedPurchases && purchases.length && selectedSite?.ID ) {
+		// TODO: add concierge banner
+		return (
+			<PurchasesSite
+				key={ selectedSite.ID }
+				siteId={ selectedSite.ID }
+				name={ selectedSite.name }
+				domain={ selectedSite.domain }
+				slug={ selectedSite.slug }
+				purchases={ purchases }
+			/>
+		);
+	}
+
+	// TODO: add concierge banner
+	return (
+		<CompactCard className="subscriptions__list--no-content">
+			<EmptyContent
+				title={ translate( 'Looking to upgrade?' ) }
+				line={ translate(
+					'Our plans give your site the power to thrive. Find the plan that works for you.'
+				) }
+				action={ translate( 'Upgrade now' ) }
+				actionURL={ '/plans' }
+				illustration={ '/calypso/images/illustrations/illustration-nosites.svg' }
+			/>
+		</CompactCard>
+	);
 }
